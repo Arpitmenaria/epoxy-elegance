a15c792b0b3e78adaaa339911710a6747528a614
Arpit <menariaarpit2@gmail.com>
2025-07-01 23:10:14 +0530

    Initial commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 EpoxyElegance. All Rights Reserved.</p>
+      <p>© 2025 EpoxyFurnish. All Rights Reserved.</p>
       <p>Address: Epoxy Elegance, 100ft road Shobhagpura, opp Historia Royal Hotel</p>
     </footer>
   );
